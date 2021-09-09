6b5878bf87041dddfab1c9a678198e752cbcd916
Artur <info@aigroupusa.com>
2021-09-09 15:01:52 -0400

    commit tree 52080bca0906c25937b64a5735ae6b4068346a78

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,13 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist!, only: %i[show destroy edit update] 
+
   def index
+    @artists = Artist.all
   end
 
   def show
+    @songs = @artist.songs
+    @song_count = @artist.song_count
   end
 
   def new
@@ -11,7 +16,6 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.new(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -20,14 +24,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -36,13 +36,16 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
   end
 
   private
+
+  def set_artist!
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name)
